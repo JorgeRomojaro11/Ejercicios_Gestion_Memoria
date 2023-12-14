7078939e4848eb6d14513a6e9e3151c534829371
JorgeRomojaro11 <jromodel@myuax.com>
2023-12-14 10:07:16 +0100

    .

--- main.cpp
+++ main.cpp
@@ -5,4 +5,17 @@
 #include <unistd.h>
 #include <sys/wait.h>
 
+#define SIZE 4096
+
+int main() {
+    char *shared_memory = (char *)mmap(NULL, SIZE, PROT_READ|PROT_WRITE, MAP_SHARED|MAP_ANONYMOUS, -1, 0);
+    if (shared_memory == MAP_FAILED) {
+        perror("mmap");
+        exit(EXIT_FAILURE);
+    }
+    pid_t pid = fork();
+
+    if (pid < 0) {
+        perror("fork");
+        exit(EXIT_FAILURE);
 
