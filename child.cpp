5d8635501161ccc9e5b41f828a400aa83f41d5b8
JorgeRomojaro11 <jromodel@myuax.com>
2023-12-14 10:12:55 +0100

    .

--- child.cpp
+++ child.cpp
@@ -7,3 +7,12 @@
 int _tmain() {
     HANDLE hMapFile;
     LPCTSTR pBuf;
+    hMapFile = OpenFileMapping(
+            FILE_MAP_ALL_ACCESS,   // access
+            FALSE,                 // do not inherit the name
+            L"MySharedMemory");
+
+    if (hMapFile == NULL) {
+        _tprintf(_T("Could not open file mapping object (%d)\n"), GetLastError());
+        return 1;
+
