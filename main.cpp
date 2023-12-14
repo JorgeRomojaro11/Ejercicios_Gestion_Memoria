984fe582af48f6eae693f5bf355a1fd7a8a7e036
JorgeRomojaro11 <jromodel@myuax.com>
2023-12-14 10:10:58 +0100

    .

--- main.cpp
+++ main.cpp
@@ -4,3 +4,15 @@
 #include <strsafe.h>
 
 #define SIZE 4096
+
+int _tmain() {
+    HANDLE hMapFile;
+    LPCTSTR pBuf;
+
+    hMapFile = CreateFileMapping(
+            INVALID_HANDLE_VALUE,
+            NULL,
+            PAGE_READWRITE,
+            0,
+            SIZE,
+            L"MySharedMemory");
